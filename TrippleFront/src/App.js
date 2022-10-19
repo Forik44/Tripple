71720fddd3b801a1582856e83ec8c3a91ce30ad6
Egor Prokofev <egor.prokofev.ru@gmail.com>
2022-10-19 22:22:38 +0300

    Added frontend 3

---
+++ TrippleFront/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header'
+import ShopListPage from './pages/ShopListPage'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <ShopListPage/>
+    </div>
+  );
+}
+
+export default App;
