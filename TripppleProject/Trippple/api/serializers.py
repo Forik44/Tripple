7ebd05f64282484da3b2a8fdcf16c8661504adfb
Aleksey Putilov <a1eksey.bru@yandex.ru>
2022-11-01 14:22:19 +0300

    Revert "Added JWT login, register, getUser"
    This reverts commit 99fc98bdfd70ec74297354b8d481d053841b8618.

--- TripppleProject/Trippple/api/serializers.py
+++ TripppleProject/Trippple/api/serializers.py
@@ -1,12 +1,8 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Product, CustomUser
+from .models import Product
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = '__all__'
